e75b68611ed927538be665130b3f6a7f93509386
Fra-Sergi <francescosergi23@gmail.com>
2023-05-20 19:00:25 +0200

    Change dataset from 2023 to 2022 -Make a Sepformer model and import in train_baseline_task1.py -Insert MIMO in SepFormer (beginning) -Print shape of STFT output

--- train_baseline_task1.py
+++ train_baseline_task1.py
@@ -147,14 +147,14 @@ def main(args):
         model = MIMO_UNet_Beamforming(fft_size=args.fft_size,
                                       hop_size=args.hop_size,
                                       input_channel=args.input_channel)
-    elif args.architecture == 'audiovisual_MIMO_Unet_Beamforming':
-        model = audiovisual_MIMO_UNet_Beamforming(fft_size=args.fft_size,
-                                      hop_size=args.hop_size,
-                                      input_channel=args.input_channel)
     elif args.architecture == 'MIMO':
         model = MIMO(fft_size=args.fft_size,
-                     hop_size=args.hop_size,
-                     input_channel=args.input_channel)
+                                      hop_size=args.hop_size,
+                                      input_channel=args.input_channel)
+    #elif args.architecture == 'MIMO':
+        #model = MIMO(fft_size=args.fft_size,
+                     #hop_size=args.hop_size,
+                     #input_channel=args.input_channel)
     if args.use_cuda:
         print("Moving model to gpu")
     model = model.to(device)
