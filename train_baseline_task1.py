45012f8c56f1083ba2a1eacea02893106699c250
Fra-Sergi <francescosergi23@gmail.com>
2023-11-05 08:56:24 +0100

    prova

--- train_baseline_task1.py
+++ train_baseline_task1.py
@@ -378,7 +378,7 @@ if __name__ == '__main__':
         wandb.init(
             # set the wandb project where this run will be logged
             #entity='thesis-i2i',
-            project="AmbiSE_different_dim",
+            project="AmbiSE_different_dim_L2",
             name="myAMMB",
             # track hyperparameters and run metadata
             config={
